08aaa32ed7721a5c2ed09fa558d741cfd72fcc75
Patrick Morrison <patgramo@gmail.com>
2022-04-14 08:19:28 -0700

    Added functions to fetch and display repos

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 //Where profile info will appear//
 const overview = document.querySelector(".overview");
 const username = "patgramo";
+const repoList = document.querySelector(".repo-list");
 
 const getInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    displayInfo(data);
+    displayUserInfo(data);
 };
 
 getInfo();
 
-const displayInfo = function(data) {
+const displayUserInfo = function(data) {
     const newDiv = document.createElement("div");
     newDiv.classList.add(".user-info");
     newDiv.innerHTML = 
@@ -24,4 +25,20 @@ const displayInfo = function(data) {
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`;
     overview.append(newDiv);
+    getRepos();
 };
+
+    const getRepos = async function() {
+        const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+        const repoData = await fetchRepos.json();
+        displayRepos(repoData);
+    };
+
+    const displayRepos = function(repos) {
+        for (const repo of repos) {
+            const repoItem = document.createElement("li");
+            repoItem.classList.add("repo");
+            repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+            repoList.append(repoItem);
+        }
+    };
